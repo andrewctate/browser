f4c3a176effcca227a1aab2c69c9c78898e9a6e9
Andrew Tate <drewctate@gmail.com>
2022-02-12 16:39:56 -0600

    Add scrolling

--- browser.py
+++ browser.py
@@ -233,7 +233,24 @@ def build_view_source_html(source: str):
     return f"<body>{escape_html(source)}</body>"
 
 
+HSTEP, VSTEP = 13, 18
+
+
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+        if cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+
+    return display_list
+
+
 WIDTH, HEIGHT = 800, 600
+SCROLL_STEP = 100
 
 
 class Browser:
@@ -241,6 +258,26 @@ class Browser:
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
         self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown)
+        self.window.bind("<Up>", self.scrollup)
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+    def scrollup(self, e):
+        self.scroll -= SCROLL_STEP
+        self.draw()
+
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c in self.display_list:
+            if y > self.scroll + HEIGHT:
+                continue
+            if y + VSTEP < self.scroll:
+                continue
+            self.canvas.create_text(x, y - self.scroll, text=c)
 
     def load(self, url: str):
         view_source = url.startswith("view-source:")
@@ -249,9 +286,8 @@ class Browser:
 
         headers, body = request(url)
         text = lex(build_view_source_html(body) if view_source else body)
-
-        for c in text:
-            self.canvas.create_text(100, 100, text=c)
+        self.display_list = layout(text)
+        self.draw()
 
 
 if __name__ == '__main__':
