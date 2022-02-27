3e23dc292d76b6094a03552e1df97e01177106dc
Andrew Tate <drewctate@gmail.com>
2022-02-27 08:34:44 -0600

    Support implicit tags

---
+++ README.md
@@ -0,0 +1,22 @@
+## Supported Features
+
+### Requests
+
+- URL schemes: `http:`, `https:` (TLS), `data:`, `file:` and `view-source:`
+- G-ZIP content encoding
+- Chunked transfer encoding
+
+### Typesetting
+
+- Supports `i`, `b`, `small`, `big`, `sup`, `br`, `p`
+- Supports HTML entities
+- Respects soft-hyphens (`&shy;`)
+
+### Window interactions
+
+- Scroll, both with mousewheel and arrow keys
+- Resizing
+
+### Misc
+
+- Implicit `html`, `head`, and `body` tags
--- parser.py
+++ parser.py
@@ -27,13 +27,35 @@ class Text:
 
 
 class HTMLParser:
+    HEAD_TAGS = [
+        "base", "basefont", "bgsound", "noscript",
+        "link", "meta", "title", "style", "script",
+    ]
+    SELF_CLOSING_TAGS = [
+        "area", "base", "br", "col", "embed", "hr", "img", "input",
+        "link", "meta", "param", "source", "track", "wbr",
+    ]
+
     def __init__(self, html):
         self.html = html
         self.unfinished = []
-        self.SELF_CLOSING_TAGS = [
-            "area", "base", "br", "col", "embed", "hr", "img", "input",
-            "link", "meta", "param", "source", "track", "wbr",
-        ]
+
+    def implicit_tags(self, tag: str):
+        while True:
+            open_tags = [node.tag for node in self.unfinished]
+            if open_tags == [] and tag != "html":
+                self.add_tag("html")
+            elif open_tags == ["html"] \
+                    and tag not in ["head", "body", "/html"]:
+                if tag in self.HEAD_TAGS:
+                    self.add_tag("head")
+                else:
+                    self.add_tag("body")
+            elif open_tags == ["html", "head"] and \
+                    tag not in ["/head"] + self.HEAD_TAGS:
+                self.add_tag("/head")
+            else:
+                break
 
     def get_attributes(self, text):
         parts = text.split()
@@ -71,6 +93,7 @@ class HTMLParser:
     def add_text(self, text):
         if text.isspace():
             return
+        self.implicit_tags(None)
         parent = self.unfinished[-1]
         node = Text(text, parent)
         parent.children.append(node)
@@ -80,6 +103,7 @@ class HTMLParser:
         # throw away doctype and comments
         if tag.startswith("!"):
             return
+        self.implicit_tags(tag)
         if tag.startswith("/"):
             if len(self.unfinished) == 1:
                 return
